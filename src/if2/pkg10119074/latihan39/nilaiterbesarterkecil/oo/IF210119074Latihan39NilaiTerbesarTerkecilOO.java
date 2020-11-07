3df9adcd9f7bdd81d786bcf94ff535513655ce9b
HandrianRivaldi74 <handrianr28@gmail.com>
2020-11-08 01:18:42 +0700

    Latihan39

---
+++ src/if2/pkg10119074/latihan39/nilaiterbesarterkecil/oo/IF210119074Latihan39NilaiTerbesarTerkecilOO.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if2.pkg10119074.latihan39.nilaiterbesarterkecil.oo;
+
+/**
+ *
+ * @author Acer
+ * NAMA      : HANDRIAN RIVALDI
+ * KELAS     : PBOIF2
+ * NIM       : 10119074
+ * Deskrifsi :
+ */
+public class IF210119074Latihan39NilaiTerbesarTerkecilOO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        NilaiTerbesarTerkecil nilaiMhs = new NilaiTerbesarTerkecil();
+        nilaiMhs.tampilNilaiMhs();
+    
+    }
+    
+}
